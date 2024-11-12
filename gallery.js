4677e23edb8cdb5e25c231a13e657c1b54828147
525santushevich <525santushevich@frhsd.com>
2024-11-12 11:55:19 -0500

    Display first image and metadata

--- gallery.js
+++ gallery.js
@@ -1,6 +1,6 @@
 let mCurrentIndex = 0; // Tracks the current image index
 let mImages = []; // Array to hold GalleryImage objects
-const mUrl = "https://your-json-url.com"; // Replace with actual JSON URL
+const mUrl = "images.json"; // Replace with actual JSON URL
 const mWaitTime = 5000; // Timer interval in milliseconds
 
 $(document).ready(() => {
@@ -23,17 +23,15 @@ $(document).ready(() => {
 // Function to fetch JSON data and store it in mImages
 function fetchJSON() {
   // Use $.ajax here to request the JSON data from mUrl
-  // On success, parse the JSON and push each image object into mImages array
   // After JSON is loaded, call swapPhoto() to display the first image
   $.ajax({
     type: "GET",
     url: mUrl,
     dataType: "json",
+    // On success, parse the JSON and push each image object into mImages array
     success: function (data) {
-      const image = data.images;
-      image.forEach((element) => {
-        mImages.push(element);
-      });
+      mImages = data.images;
+      // After JSON is loaded, call swapPhoto() to display the first image
       swapPhoto();
     },
   });
@@ -41,6 +39,8 @@ function fetchJSON() {
 
 // Function to swap and display the next photo in the slideshow
 function swapPhoto() {
+  let currentImg = mImages[mCurrentIndex];
+  $("#photo").attr("src", "/img/places/usa.jpg");
   // Access mImages[mCurrentIndex] to update the image source and details
   // Update the #photo element's src attribute with the current image's path
   // Update the .location, .description, and .date elements with the current image's details
